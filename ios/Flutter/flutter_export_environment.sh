1c5a05bf6bcfa90b5eff2d089bd5258dac32ef31
OsmarVitor <perez_vitor@hotmail.com>
2020-07-09 01:05:46 -0300

    Adicionando Firebase Authentication no login

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/vitorperez/development/flutter-config"
-export "FLUTTER_APPLICATION_PATH=/Users/vitorperez/Documents/flutter/quiz-learn-clojure"
-export "FLUTTER_TARGET=/Users/vitorperez/Documents/flutter/quiz-learn-clojure/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/vitorperez/Documents/quiz-learn-clojure"
+export "FLUTTER_TARGET=/Users/vitorperez/Documents/quiz-learn-clojure/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
@@ -10,3 +10,4 @@ export "FLUTTER_FRAMEWORK_DIR=/Users/vitorperez/development/flutter-config/bin/c
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "TRACK_WIDGET_CREATION=true"
+export "DART_DEFINES=flutter.inspector.structuredErrors=true"
--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'home.dart';
 
 class Login extends StatefulWidget {
@@ -7,20 +7,41 @@ class Login extends StatefulWidget {
   _Login createState() => _Login();
 }
 
-class _Login extends State<Login> {
+void singIn(String email, String password, context) async {
+  try {
+    AuthResult response = await FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email, password: password);
+    if (response.user.uid != null) {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => Home()));
+    }
+  } catch (e) {
+    print('Error $e');
+  }
+}
 
-  TextEditingController login = new TextEditingController();
+void createUser(String email, String password, context) async {
+  AuthResult user = await FirebaseAuth.instance
+      .createUserWithEmailAndPassword(email: email, password: password);
+  if (user.user.email != null) {
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => Home()));
+  }
+}
+
+class _Login extends State<Login> {
+  TextEditingController email = new TextEditingController();
   TextEditingController password = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     final loginField = TextFormField(
-      controller: login,
+      controller: email,
       obscureText: false,
       decoration: InputDecoration(
-          contentPadding: EdgeInsets.fromLTRB(20.0, 0.0,0.0, 0.0),
+          contentPadding: EdgeInsets.fromLTRB(20.0, 0.0, 0.0, 0.0),
           hintText: "fatec",
-          labelText: "Login",
+          labelText: "Email",
           border:
               OutlineInputBorder(borderRadius: BorderRadius.circular(30.0))),
     );
@@ -28,7 +49,6 @@ class _Login extends State<Login> {
     final passwordField = TextFormField(
       controller: password,
       obscureText: true,
-
       decoration: InputDecoration(
           contentPadding: EdgeInsets.fromLTRB(20.0, 0.0, 0.0, 0.0),
           hintText: "fatec",
@@ -40,45 +60,94 @@ class _Login extends State<Login> {
     final buttonLogin = ButtonTheme(
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-        child: RaisedButton(
-            color: Colors.green,
-            textColor: Colors.white,
-            child: Text("Entrar",
-                textAlign: TextAlign.center),
-            onPressed: () {
-              if(login.text == "fatec" && password.text == "fatec") {
-                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => Home()));
-              }
-            },
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30.0))));
+        child: Column(children: <Widget>[
+          Container(
+              child: RaisedButton(
+                  color: Colors.green,
+                  textColor: Colors.white,
+                  child: Text("Entrar", textAlign: TextAlign.center),
+                  onPressed: () {
+                    singIn(email.text, password.text, context);
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                              title:
+                                  Text("Acesso inválido, cadastre-se abaixo"),
+                              actions: <Widget>[
+                                FlatButton(
+                                    child: Text("Ok"),
+                                    onPressed: () {
+                                      Navigator.pushReplacement(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => Login()));
+                                    })
+                              ]);
+                        });
+                  },
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0)))),
+          Container(
+            child: RaisedButton(
+                color: Colors.green,
+                textColor: Colors.white,
+                child: Text("Cadastrar", textAlign: TextAlign.center),
+                onPressed: () {
+                  createUser(email.text, password.text, context);
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                            title: Text(
+                                "Usuário criado! Clique em Ok para entrar no Quiz"),
+                            actions: <Widget>[
+                              FlatButton(
+                                  child: Text("Ok"),
+                                  onPressed: () {
+                                    Navigator.pushReplacement(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => Login()));
+                                  })
+                            ]);
+                      });
+                },
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0))),
+          )
+        ]));
 
     return Scaffold(
-      backgroundColor: Colors.grey[900],
+        backgroundColor: Colors.grey[900],
         appBar: AppBar(
             backgroundColor: Colors.grey[700], title: Text('Learn Clojure!')),
         body: Center(
             child: SingleChildScrollView(
           child: Container(
-            margin: new EdgeInsets.symmetric(horizontal: 50.0, vertical: 160.0),
-            decoration: BoxDecoration(
-              color: Colors.purple[800],
-              borderRadius: BorderRadius.circular(12)),
+              margin:
+                  new EdgeInsets.symmetric(horizontal: 50.0, vertical: 160.0),
+              decoration: BoxDecoration(
+                  color: Colors.purple[800],
+                  borderRadius: BorderRadius.circular(12)),
               padding: const EdgeInsets.all(10.0),
               child: Column(
                 children: <Widget>[
                   SizedBox(
                     height: 150.0,
-                    child:
-                        Image.asset("assets/logo.png", width:100),
+                    child: Image.asset("assets/logo.png", width: 100),
                   ),
-                  SizedBox(width: 900,
-                              height: 40,),
+                  SizedBox(
+                    width: 900,
+                    height: 40,
+                  ),
                   loginField,
                   SizedBox(height: 10.0),
                   passwordField,
-                  SizedBox(width: 900,
-                              height: 40,),
+                  SizedBox(
+                    width: 900,
+                    height: 40,
+                  ),
                   buttonLogin,
                 ],
               )),
